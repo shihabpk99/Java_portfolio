9dfc821ea76cf7cc1075f9edba298325a584d1af
shihabpk99 <shihabkr9@gmail.com>
2025-12-16 23:36:03 +0600

    Support multiple students using ArrayList

--- student-management-system/src/Main.java
+++ student-management-system/src/Main.java
@@ -1,10 +1,15 @@
 public class Main {
     public static void main(String[] args) {
-        Student s1 = new Student(1, "Shihab", 3.0);
-        s1.displayInfo();
 
         StudentManager manager = new StudentManager();
+
+        Student s1 = new Student(1, "Shihab", 3.0);
+        Student s2 = new Student(2, "Araf", 3.4);
+
         manager.addStudent(s1);
+        manager.addStudent(s2);
+
+        System.out.println("All Students:");
+        manager.displayAllStudents();
     }
-    
 }
--- student-management-system/src/StudentManager.java
+++ student-management-system/src/StudentManager.java
@@ -1,9 +1,23 @@
+import java.util.ArrayList;
+
 public class StudentManager {
+
+    private ArrayList<Student> students = new ArrayList<>();
+
     public void addStudent(Student student) {
-        // Code to add student
-        System.out.println("Student added: ");
-        student.displayInfo();
+        students.add(student);
+        System.out.println("Student added successfully.\n");
     }
 
-    
+    public void displayAllStudents() {
+        if (students.isEmpty()) {
+            System.out.println("No students found.");
+            return;
+        }
+
+        for (Student s : students) {
+            s.displayInfo();
+            System.out.println("------");
+        }
+    }
 }
