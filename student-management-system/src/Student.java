186ed81f6bb54369fd588b48ee2d93dcb054b627
shihabpk99 <shihabkr9@gmail.com>
2026-01-19 13:00:04 +0600

    Refactor Student model with documentation and toString

--- student-management-system/src/Student.java
+++ student-management-system/src/Student.java
@@ -1,8 +1,15 @@
+
+ //Represents a student with basic academic information.
+
 public class Student {
+
     private int id;
     private String name;
     private double cgpa;
 
+    
+     //Creates a new Student object.
+     
     public Student(int id, String name, double cgpa) {
         this.id = id;
         this.name = name;
@@ -21,9 +28,22 @@ public class Student {
         return cgpa;
     }
 
+    
+     //Displays student information in a readable format.
+     
     public void displayInfo() {
-        System.out.println("ID: " + id);
-        System.out.println("Name: " + name);
-        System.out.println("CGPA: " + cgpa);
+        System.out.println("ID   : " + id);
+        System.out.println("Name : " + name);
+        System.out.println("CGPA : " + cgpa);
+    }
+
+    
+     // Returns student information as a string. Useful for debugging and logging.
+    
+    @Override
+    public String toString() {
+        return "Student{id=" + id +
+                ", name='" + name + '\'' +
+                ", cgpa=" + cgpa + '}';
     }
 }
